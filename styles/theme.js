31e415c7c6be6ccb045e99a213f12d6c6ef8b8ae
orellanarodrigo <rodrigo.orellana.1982@gmail.com>
2023-01-31 23:46:25 -0300

    fix clasess error and fix theme

--- styles/theme.js
+++ styles/theme.js
@@ -9,6 +9,10 @@ const palette = {
   secondary: {
     main: '#1DA1F2',
     dark: '#52575D',
+    opacity: 'rgba(82, 87, 93, 0.30)',
+    blueOpacity: 'rgba(29, 161, 242, 0.30)',
+    greenOpacity: 'rgba(5, 192, 137, 0.8)',
+    yellowOpacity: 'rgba(255, 182, 23, 0.30)',
     light: '#FFFFFF',
     yellow: '#FFB617',
     facebook: '#3b5998',
@@ -33,18 +37,27 @@ const theme = createTheme({
     body1: {
       fontWeight: 300,
       fontSize: 18,
+      color: palette.secondary.dark,
     },
     body2: {
-      fontWeight: 300,
+      fontWeight: 500,
       fontSize: 15,
+      color: palette.secondary.dark,
     },
     h2: {
-      fontWeight: 600,
-      fontSize: 25,
+      fontWeight: 800,
+      color: palette.secondary.dark,
+      fontSize: 20,
     },
     h3: {
-      fontWeight: 600,
-      fontSize: 20,
+      fontWeight: 500,
+      color: palette.secondary.dark,
+      fontSize: '1.1rem',
+    },
+    h4: {
+      fontWeight: 300,
+      fontSize: 15,
+      color: palette.secondary.dark,
     },
     cameraIcon: {
       size: 40,
@@ -57,6 +70,29 @@ const theme = createTheme({
       '@global': {
         body: {
           color: palette.secondary.dark,
+        },
+        h2: {
+          margin: 0,
+        },
+        h3: {
+          fontWeight: 400,
+          fontSize: 18,
+          margin: 0,
+        },
+        p: {
+          margin: 0,
+        },
+      },
+    },
+    MuiTooltip: {
+      tooltip: {
+        backgroundColor: '#1DA1F2',
+        top: -14,
+        position: 'relative',
+        '@media only screen and (max-width: 600px)': {
+          top: -30,
+          fontSize: '10px !important',
+          padding: '2px !important',
         },
       },
     },
